03d72f5f203b28cce21e50e2abe9b78996cd4312
Hugo <hugobrinkcarvalho@gmail.com>
2024-10-21 11:19:44 +0100

    feat(projects): add project section with last projects

---
+++ src/content/last_projects.js
@@ -0,0 +1,111 @@
+export const last_projects = [
+	{
+		organization: 'Database - Mocamfe',
+		type: 'web app',
+		avatar: '/img/last_projects/mocamfe.png',
+		description:
+			'Platform to manage member activities, payments, and receipts',
+		list: [
+			'Built with Remix, TailwindCSS, Prisma and PostgresSQL',
+			'Used Ag-grid for the tables',
+			'Used Zod for data validation',
+		],
+		techs: {
+			Remix: 'skill-icons:remix-light',
+			TailwindCSS: 'skill-icons:tailwindcss-light',
+			Prisma: 'vscode-icons:file-type-prisma',
+			PostgresSQL: 'logos:postgresql',
+		},
+	},
+	{
+		organization: 'Mediterraned',
+		type: 'website',
+		avatar: '/img/last_projects/mediterraned.png',
+		link: 'https://www.mediterraned.org/',
+		description: 'Dynamic and responsive website for Mediterraned',
+		list: [
+			'Built with NextJS, TailwindCSS',
+			'Used Wordpress as CMS',
+			'Applied Figma Mockups',
+		],
+
+		techs: {
+			NextJS: 'logos:nextjs-icon',
+			TailwindCSS: 'skill-icons:tailwindcss-light',
+			Figma: 'skill-icons:figma-light',
+			Wordpress: 'skill-icons:wordpress',
+		},
+	},
+	{
+		organization: 'Credential Platform - Queima das Fitas',
+		type: 'web app / mobile app',
+		description:
+			"Credential management system for one of Portugal's largest festivals",
+		list: ['Built with Python, Flask, React and PostgresSQL'],
+		techs: {
+			Python: 'skill-icons:python-light',
+			Flask: 'skill-icons:flask-light',
+			React: 'logos:react',
+			PostgresSQL: 'logos:postgresql',
+		},
+	},
+
+	{
+		organization: 'Online Gastcolleges',
+		type: 'website',
+		avatar: '/img/last_projects/onlineGastcolleges.png',
+		link: 'https://onlinegastcolleges.netlify.app/',
+		description:
+			'Platform to host videos and provide information on upcoming Dutch language events',
+		list: [
+			'Built with React, TailwindCSS',
+			'Focused on clean design, responsive layouts, and fast load times',
+			'Easy to navigate and find the information you need',
+		],
+		techs: {
+			React: 'logos:react',
+			TailwindCSS: 'skill-icons:tailwindcss-light',
+			Git: 'logos:git-icon',
+		},
+	},
+	{
+		organization: 'Personal Websites',
+		type: 'website',
+		link: 'https://antoinetbrink.vercel.app/',
+		avatar: 'img/last_projects/antoinetBrink.png',
+		description:
+			'Developed a clean, responsive personal website focused on intuitive navigation and design, ensuring easy access to relevant information.',
+		list: [
+			'Built with Next.js, TailwindCSS',
+			'Focused on clean design, responsive layouts, and fast load times',
+			'Hosted on Vercel',
+		],
+		techs: {
+			NextJS: 'logos:nextjs-icon',
+			TailwindCSS: 'skill-icons:tailwindcss-light',
+			Vercel: 'logos:vercel-icon',
+			Git: 'logos:git-icon',
+		},
+	},
+	{
+		organization: 'Portfolio Website',
+		type: 'website',
+		avatar: 'img/last_projects/portfolio.png',
+		link: '/',
+		github: 'https://github.com/HugoBrink/sleepy-shell',
+		description: 'Developed a website with Astro to showcase my projects. ',
+
+		list: [
+			'Built with Astro, TailwindCSS',
+			'Focused on clean design, responsive layouts, and fast load times',
+			'Used Figma for the design',
+			'Hosted on Vercel',
+		],
+		techs: {
+			Astro: 'logos:astro-icon',
+			Figma: 'skill-icons:figma-light',
+			React: 'logos:react',
+			TailwindCSS: 'skill-icons:tailwindcss-light',
+		},
+	},
+];
